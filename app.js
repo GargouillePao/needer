916c91fe3db5af29f2c84e7c330bb4e924b9e6b6
Gargouille <792459576@qq.com>
2016-03-04 16:26:20 +0800

    new project

---
+++ app.js
@@ -0,0 +1,8 @@
+
+//var config = require("./config")();
+var app = require("./init")();
+//app.configure(config);
+//app.initRouter("/index");
+//app.initSocket("index");
+//app.initSocket("others");
+app.initServer();
